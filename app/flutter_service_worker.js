06cb337307f8bb09fa7ee384a4c1a51e279e9b97
Janco-SciGrow <janco@scigrow.tech>
2025-11-19 14:38:53 +0200

    Fix service worker

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -207,3 +207,23 @@ function onlineFirst(event) {
     })
   );
 }
+
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyC8FsxZw5yqH7sq0QjZHvRGs_HjP4nwi4U",
+    authDomain: "trompsburg-buurtwag.firebaseapp.com",
+    projectId: "trompsburg-buurtwag",
+    storageBucket: "trompsburg-buurtwag.appspot.com",
+    messagingSenderId: "925271604221",
+    appId: "1:925271604221:web:dee9a4d22c05e4aecbb794",
+    measurementId: "G-DDDG2HTSEJ"
+});
+// Necessary to receive background messages:
+const messaging = firebase.messaging();
+
+// Optional:
+// messaging.onBackgroundMessage((message) => {
+//   console.log("onBackgroundMessage", message);
+// });
